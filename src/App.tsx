582ca10283703c95d136ad04edac35be99f1713a
leonraz58 <leonraz58@gmail.com>
2024-06-04 19:23:50 +0300

    completed

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import styled, {keyframes} from 'styled-components';
 
 const Navigation = () => {
     return (
-        <Menu>
-            <MenuItem>Item 1</MenuItem>
-            <MenuItem>Item 2</MenuItem>
-            <MenuItem>Item 3</MenuItem>
-        </Menu>
+        <nav>
+            <Menu role={"menu"}>
+                <MenuItem role={"menuitem"}><a href="#">Item 1</a></MenuItem>
+                <MenuItem role={"menuitem"}><a href="#">Item 2</a></MenuItem>
+                <MenuItem role={"menuitem"}><a href="#">Item 3</a></MenuItem>
+            </Menu>
+        </nav>
+
     );
 };
 
@@ -17,24 +20,26 @@ function App() {
         <div className="App">
             <Header>
                 <Navigation/>
-                <LoginBtn>LOGIN</LoginBtn>
+                <LoginBtn accessKey={'x'} aria-label={"login"}>LOGIN</LoginBtn>
             </Header>
             <Block>
                 <Title>Accessibility</Title>
                 <Form>
                     <div>
-                        <Label htmlFor="1">Label for field 1</Label>
-                        <Field id="1" placeholder="Field 1"/>
+                        {/*aria-hidden - не читать элемент*/}
+                        <Label aria-hidden htmlFor="1">Label for field 1</Label>
+                        {/*плейсхолдеры скринридер видит*/}
+                        <Field tabIndex={1} id="1" placeholder="Hello my friend"/>
                     </div>
 
                     <div>
                         <Label htmlFor="2">Label for field 2</Label>
-                        <Field id="2" placeholder="Field 2"/>
+                        <Field tabIndex={3} id="2" placeholder="Field 2"/>
                     </div>
 
                     <div>
                         <Label htmlFor="3">Label for field 3</Label>
-                        <Field id="3" placeholder="Field 3"/>
+                        <Field tabIndex={2} id="3" placeholder="Field 3"/>
                     </div>
                 </Form>
             </Block>
@@ -51,32 +56,41 @@ const Bouncing = keyframes`
   }  
 `
 
-const Title = styled.div`
+const Title = styled.h1`
   font-size: 1.5em;
   text-align: center;
   margin: 60px 0;
   animation: ${Bouncing} 1s linear infinite alternate;
+
+    //@media (prefers-reduced-motion: reduce) {
+    //    animation: none;
+    //}
 `;
 
-const Header = styled.div`
-  height: 80px;
-  background-color: #e91e63;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
+const Header = styled.header`
+    height: 80px;
+    background-color: #e91e63;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    button {
+        background-color: transparent;
+    }
 `;
 
-const Menu = styled.div`
-  display: flex;
-  gap: 15px;
+const Menu = styled.ul`
+    display: flex;
+    gap: 15px;
+    list-style: none;
 `;
 
-const MenuItem = styled.div`
+const MenuItem = styled.li`
   color: white;
   font-weight: bold;
 `;
 
-const LoginBtn = styled.div`
+const LoginBtn = styled.button`
   color: white;
   font-weight: bold;
   padding: 10px 20px;
@@ -84,14 +98,14 @@ const LoginBtn = styled.div`
   border-radius: 5px;
 `;
 
-const Block = styled.div`
+const Block = styled.section`
   flex-grow: 1;
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
 
-const Form = styled.div`
+const Form = styled.form`
   width: 100%;
   max-width: 500px;
   display: flex;
